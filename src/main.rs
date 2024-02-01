310833bfb876e10d6988c2cf49707668982167d0
小煜 <ow666faka@qq.com>
2024-02-01 20:32:13 +0800

    update vertex

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 //! A shader that uses the GLSL shading language.
 
 use bevy::{
+    pbr::{MaterialPipeline, MaterialPipelineKey},
     prelude::*,
     reflect::TypePath,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::{
+        mesh::MeshVertexBufferLayout,
+        render_resource::{
+            AsBindGroup, RenderPipelineDescriptor, ShaderRef, SpecializedMeshPipelineError,
+        },
+    },
 };
-use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
-use bevy_screen_diagnostics::{ScreenDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin};
 
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins,
-            MaterialPlugin::<CustomMaterial>::default(),
-            MaterialPlugin::<LightMaterial>::default(),
-        ))
-        .add_plugins(ScreenDiagnosticsPlugin::default())
-        .add_plugins(ScreenFrameDiagnosticsPlugin)
-        .add_plugins(PanOrbitCameraPlugin)
+        .add_plugins((DefaultPlugins, MaterialPlugin::<CustomMaterial>::default()))
         .add_systems(Startup, setup)
         .run();
 }
@@ -26,52 +23,34 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    materials: (
-        ResMut<Assets<CustomMaterial>>,
-        ResMut<Assets<LightMaterial>>,
-    ),
+    mut materials: ResMut<Assets<CustomMaterial>>,
 ) {
-    let mut custom_material = materials.0;
-    let mut light_material = materials.1;
-
     // cube
     commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(Mesh::from(shape::Box::new(1.0, 1.0, 1.0))),
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        material: custom_material.add(CustomMaterial {
-            color: Color::Rgba {
-                red: 1.0,
-                green: 0.5,
-                blue: 0.3,
-                alpha: 1.00,
-            },
-            light_color: Color::Rgba {
-                red: 1.0,
-                green: 1.0,
-                blue: 1.0,
-                alpha: 1.00,
-            },
+        material: materials.add(CustomMaterial {
+            color: Color::BLUE,
             alpha_mode: AlphaMode::Blend,
         }),
         ..default()
     });
 
-    // light
-    commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.2 })),
-        transform: Transform::from_xyz(1.2, 1.0, 2.0),
-        material: light_material.add(LightMaterial {}),
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 9000.0,
+            range: 100.,
+            ..default()
+        },
+        transform: Transform::from_xyz(8.0, 16.0, 8.0),
         ..default()
     });
 
     // camera
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        },
-        PanOrbitCamera::default(),
-    ));
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
 // This is the struct that will be passed to your shader
@@ -79,8 +58,6 @@ fn setup(
 pub struct CustomMaterial {
     #[uniform(0)]
     color: Color,
-    #[uniform(0)]
-    light_color: Color,
     alpha_mode: AlphaMode,
 }
 
@@ -88,20 +65,29 @@ pub struct CustomMaterial {
 /// You only need to implement functions for features that need non-default behavior. See the Material api docs for details!
 /// When using the GLSL shading language for your shader, the specialize method must be overridden.
 impl Material for CustomMaterial {
+    fn vertex_shader() -> ShaderRef {
+        "shaders/custom_material.vert".into()
+    }
+
     fn fragment_shader() -> ShaderRef {
-        "shaders/custom_material.wgsl".into()
+        "shaders/custom_material.frag".into()
     }
 
     fn alpha_mode(&self) -> AlphaMode {
         self.alpha_mode
     }
-}
 
-#[derive(Asset, TypePath, AsBindGroup, Clone)]
-pub struct LightMaterial {}
-
-impl Material for LightMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/lighting_material.wgsl".into()
+    // Bevy assumes by default that vertex shaders use the "vertex" entry point
+    // and fragment shaders use the "fragment" entry point (for WGSL shaders).
+    // GLSL uses "main" as the entry point, so we must override the defaults here
+    fn specialize(
+        _pipeline: &MaterialPipeline<Self>,
+        descriptor: &mut RenderPipelineDescriptor,
+        _layout: &MeshVertexBufferLayout,
+        _key: MaterialPipelineKey<Self>,
+    ) -> Result<(), SpecializedMeshPipelineError> {
+        descriptor.vertex.entry_point = "main".into();
+        descriptor.fragment.as_mut().unwrap().entry_point = "main".into();
+        Ok(())
     }
 }
